d73e8dd73074a4b1de66fc52e30b9fdd08cf95a0
PAGF188 <49513493+PAGF188@users.noreply.github.com>
2020-10-15 00:27:26 +0200

    Add files via upload

---
+++ headerFiles/Definiciones.h
@@ -0,0 +1,9 @@
+/**
+ * Autor: Pablo García Fernández.
+ * Archivo: Definiciones.h
+ * Versión: 1.0
+ * Descripción: Define un valor entero para cada uno de los componentes léxicos.
+ * Funciones:
+ *       - Asociar cada componente léxico con un valor distintivo.
+ *       - Exportar esta asociación para el resto de archivos.
+ */
